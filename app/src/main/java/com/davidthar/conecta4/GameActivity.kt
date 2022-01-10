69b843139e659c2299c82f6985d4e479822fdb62
davidthar <47674026+davidthar@users.noreply.github.com>
2022-01-10 13:23:23 +0100

    Konfetti añadido

--- app/src/main/java/com/davidthar/conecta4/GameActivity.kt
+++ app/src/main/java/com/davidthar/conecta4/GameActivity.kt
@@ -1,16 +1,25 @@
 package com.davidthar.conecta4
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import com.davidthar.conecta4.databinding.ActivityGameBinding
+import nl.dionsegijn.konfetti.KonfettiView
+import nl.dionsegijn.konfetti.models.Shape
+import nl.dionsegijn.konfetti.models.Size
 
 private lateinit var binding : ActivityGameBinding
 
 private var turn = 1
 private var redScore = 0
 private var yellowScore = 0
+
+private var MAX_X = 6
+private var MAX_Y = 5
+private var MIN_XY = 0
+
 private lateinit var arrayArrows : Array<ImageView>
 private lateinit var arrayBoard : Array<Array<Square>>
 
@@ -23,9 +32,185 @@ class GameActivity : AppCompatActivity() {
         setArrays()
         setButtons()
         setArrows()
+    }
 
-        
+    private fun checkWin(x : Int, y : Int, color : Int){
+        var countHorizontal = 1
+        var countVertical = 1
+        var countDiagonal1 = 1
+        var countDiagonal2 = 1
 
+        //Left
+        if(x-1>=MIN_XY && arrayBoard[x-1][y].color == color){
+            countHorizontal++
+            if(x-2>=MIN_XY && arrayBoard[x-2][y].color == color){
+                countHorizontal++
+                if(x-3>=MIN_XY && arrayBoard[x-3][y].color == color) countHorizontal++
+            }
+        }
+
+        //Right
+        if(x+1<= MAX_X && arrayBoard[x+1][y].color == color){
+            countHorizontal++
+            if(x+2<= MAX_X && arrayBoard[x+2][y].color == color){
+                countHorizontal++
+                if(x+3<= MAX_X && arrayBoard[x+3][y].color == color) countHorizontal++
+            }
+        }
+
+        //Top
+        if(y-1>=MIN_XY && arrayBoard[x][y-1].color == color){
+            countVertical++
+            if(y-2>=MIN_XY && arrayBoard[x][y-2].color == color){
+                countVertical++
+                if(y-3>=MIN_XY && arrayBoard[x][y-3].color == color) countVertical++
+            }
+        }
+
+        //Down
+        if(y+1<=MAX_Y && arrayBoard[x][y+1].color == color){
+            countVertical++
+            if(y+2<=MAX_Y && arrayBoard[x][y+2].color == color){
+                countVertical++
+                if(y+3<=MAX_Y && arrayBoard[x][y+3].color == color) countVertical++
+            }
+        }
+
+        //Left-Top
+        if(x-1>=MIN_XY && y-1>= MIN_XY && arrayBoard[x-1][y-1].color == color){
+            countDiagonal1++
+            if(x-2>=MIN_XY && y-2>= MIN_XY && arrayBoard[x-2][y-2].color == color){
+                countDiagonal1++
+                if(x-3>=MIN_XY && y-3>= MIN_XY && arrayBoard[x-3][y-3].color == color) countDiagonal1++
+            }
+        }
+
+        //Right-Top
+        if(x+1<= MAX_X && y-1>= MIN_XY && arrayBoard[x+1][y-1].color == color){
+            countDiagonal2++
+            if(x+2<= MAX_X && y-2>= MIN_XY && arrayBoard[x+2][y-2].color == color){
+                countDiagonal2++
+                if(x+3<= MAX_X && y-3>= MIN_XY && arrayBoard[x+3][y-3].color == color) countDiagonal2++
+            }
+        }
+
+        //Right-Down
+        if(x+1<= MAX_X && y+1<= MAX_Y && arrayBoard[x+1][y+1].color == color){
+            countDiagonal1++
+            if(x+2<= MAX_X && y+2<= MAX_Y && arrayBoard[x+2][y+2].color == color){
+                countDiagonal1++
+                if(x+3<= MAX_X && y+3<= MAX_Y && arrayBoard[x+3][y+3].color == color) countDiagonal1++
+            }
+        }
+
+        //Left-Down
+        if(x-1>=MIN_XY && y+1<= MAX_Y && arrayBoard[x-1][y+1].color == color){
+            countDiagonal2++
+            if(x-2>=MIN_XY && y+2<= MAX_Y && arrayBoard[x-2][y+2].color == color){
+                countDiagonal2++
+                if(x-3>=MIN_XY && y+3<= MAX_Y && arrayBoard[x-3][y+3].color == color) countDiagonal2++
+            }
+        }
+
+
+        if(countHorizontal>=4 || countVertical>=4 || countDiagonal1 >=4 || countDiagonal2 >= 4){
+            setKonfetti(binding.viewKonfetti,color)
+            println("WIN")
+            when(color){
+                1 -> redScore++
+                2 -> yellowScore++
+            }
+            setScore()
+        }
+    }
+
+    private fun setArrows() {
+        arrayArrows[0].setOnClickListener {
+            when {
+                arrayBoard[0][5].color == 0 -> setPiece(0,5,turn)
+                arrayBoard[0][4].color == 0 -> setPiece(0,4,turn)
+                arrayBoard[0][3].color == 0 -> setPiece(0,3,turn)
+                arrayBoard[0][2].color == 0 -> setPiece(0,2,turn)
+                arrayBoard[0][1].color == 0 -> setPiece(0,1,turn)
+                arrayBoard[0][0].color == 0 -> setPiece(0,0,turn)
+            }
+        }
+        arrayArrows[1].setOnClickListener {
+            when {
+                arrayBoard[1][5].color == 0 -> setPiece(1,5,turn)
+                arrayBoard[1][4].color == 0 -> setPiece(1,4,turn)
+                arrayBoard[1][3].color == 0 -> setPiece(1,3,turn)
+                arrayBoard[1][2].color == 0 -> setPiece(1,2,turn)
+                arrayBoard[1][1].color == 0 -> setPiece(1,1,turn)
+                arrayBoard[1][0].color == 0 -> setPiece(1,0,turn)
+            }
+            
+        }
+        arrayArrows[2].setOnClickListener {
+            when {
+                arrayBoard[2][5].color == 0 -> setPiece(2,5,turn)
+                arrayBoard[2][4].color == 0 -> setPiece(2,4,turn)
+                arrayBoard[2][3].color == 0 -> setPiece(2,3,turn)
+                arrayBoard[2][2].color == 0 -> setPiece(2,2,turn)
+                arrayBoard[2][1].color == 0 -> setPiece(2,1,turn)
+                arrayBoard[2][0].color == 0 -> setPiece(2,0,turn)
+            }
+            
+        }
+        arrayArrows[3].setOnClickListener {
+            when {
+                arrayBoard[3][5].color == 0 -> setPiece(3,5,turn)
+                arrayBoard[3][4].color == 0 -> setPiece(3,4,turn)
+                arrayBoard[3][3].color == 0 -> setPiece(3,3,turn)
+                arrayBoard[3][2].color == 0 -> setPiece(3,2,turn)
+                arrayBoard[3][1].color == 0 -> setPiece(3,1,turn)
+                arrayBoard[3][0].color == 0 -> setPiece(3,0,turn)
+            }
+            
+        }
+        arrayArrows[4].setOnClickListener {
+            when {
+                arrayBoard[4][5].color == 0 -> setPiece(4,5,turn)
+                arrayBoard[4][4].color == 0 -> setPiece(4,4,turn)
+                arrayBoard[4][3].color == 0 -> setPiece(4,3,turn)
+                arrayBoard[4][2].color == 0 -> setPiece(4,2,turn)
+                arrayBoard[4][1].color == 0 -> setPiece(4,1,turn)
+                arrayBoard[4][0].color == 0 -> setPiece(4,0,turn)
+            }
+            
+        }
+        arrayArrows[5].setOnClickListener {
+            when {
+                arrayBoard[5][5].color == 0 -> setPiece(5,5,turn)
+                arrayBoard[5][4].color == 0 -> setPiece(5,4,turn)
+                arrayBoard[5][3].color == 0 -> setPiece(5,3,turn)
+                arrayBoard[5][2].color == 0 -> setPiece(5,2,turn)
+                arrayBoard[5][1].color == 0 -> setPiece(5,1,turn)
+                arrayBoard[5][0].color == 0 -> setPiece(5,0,turn)
+            }
+            
+        }
+        arrayArrows[6].setOnClickListener {
+            when {
+                arrayBoard[6][5].color == 0 -> setPiece(6,5,turn)
+                arrayBoard[6][4].color == 0 -> setPiece(6,4,turn)
+                arrayBoard[6][3].color == 0 -> setPiece(6,3,turn)
+                arrayBoard[6][2].color == 0 -> setPiece(6,2,turn)
+                arrayBoard[6][1].color == 0 -> setPiece(6,1,turn)
+                arrayBoard[6][0].color == 0 -> setPiece(6,0,turn)
+            }
+            
+        }
+    }
+
+    private fun setPiece(x : Int, y : Int, color : Int){
+        arrayBoard[x][y].color = color
+        when(color){
+            1 -> arrayBoard[x][y].image.setImageResource(R.drawable.ficha_roja)
+            2 -> arrayBoard[x][y].image.setImageResource(R.drawable.ficha_amarilla)
+        }
+        checkWin(x,y,color)
+        changeTurn()
     }
 
     private fun changeTurn(){
@@ -34,96 +219,6 @@ class GameActivity : AppCompatActivity() {
             1 -> binding.btnTurn.backgroundTintList = ContextCompat.getColorStateList(this,R.color.red_piece)
             2 -> binding.btnTurn.backgroundTintList = ContextCompat.getColorStateList(this,R.color.yellow_piece)
         }
-    }
-
-    private fun setArrows() {
-        arrayArrows[0].setOnClickListener {
-            when {
-                arrayBoard[0][5].color == 0 -> setPiece(arrayBoard[0][5],turn)
-                arrayBoard[0][4].color == 0 -> setPiece(arrayBoard[0][4],turn)
-                arrayBoard[0][3].color == 0 -> setPiece(arrayBoard[0][3],turn)
-                arrayBoard[0][2].color == 0 -> setPiece(arrayBoard[0][2],turn)
-                arrayBoard[0][1].color == 0 -> setPiece(arrayBoard[0][1],turn)
-                arrayBoard[0][0].color == 0 -> setPiece(arrayBoard[0][0],turn)
-            }
-        }
-        arrayArrows[1].setOnClickListener {
-            when {
-                arrayBoard[1][5].color == 0 -> setPiece(arrayBoard[1][5],turn)
-                arrayBoard[1][4].color == 0 -> setPiece(arrayBoard[1][4],turn)
-                arrayBoard[1][3].color == 0 -> setPiece(arrayBoard[1][3],turn)
-                arrayBoard[1][2].color == 0 -> setPiece(arrayBoard[1][2],turn)
-                arrayBoard[1][1].color == 0 -> setPiece(arrayBoard[1][1],turn)
-                arrayBoard[1][0].color == 0 -> setPiece(arrayBoard[1][0],turn)
-            }
-            
-        }
-        arrayArrows[2].setOnClickListener {
-            when {
-                arrayBoard[2][5].color == 0 -> setPiece(arrayBoard[2][5],turn)
-                arrayBoard[2][4].color == 0 -> setPiece(arrayBoard[2][4],turn)
-                arrayBoard[2][3].color == 0 -> setPiece(arrayBoard[2][3],turn)
-                arrayBoard[2][2].color == 0 -> setPiece(arrayBoard[2][2],turn)
-                arrayBoard[2][1].color == 0 -> setPiece(arrayBoard[2][1],turn)
-                arrayBoard[2][0].color == 0 -> setPiece(arrayBoard[2][0],turn)
-            }
-            
-        }
-        arrayArrows[3].setOnClickListener {
-            when {
-                arrayBoard[3][5].color == 0 -> setPiece(arrayBoard[3][5],turn)
-                arrayBoard[3][4].color == 0 -> setPiece(arrayBoard[3][4],turn)
-                arrayBoard[3][3].color == 0 -> setPiece(arrayBoard[3][3],turn)
-                arrayBoard[3][2].color == 0 -> setPiece(arrayBoard[3][2],turn)
-                arrayBoard[3][1].color == 0 -> setPiece(arrayBoard[3][1],turn)
-                arrayBoard[3][0].color == 0 -> setPiece(arrayBoard[3][0],turn)
-            }
-            
-        }
-        arrayArrows[4].setOnClickListener {
-            when {
-                arrayBoard[4][5].color == 0 -> setPiece(arrayBoard[4][5],turn)
-                arrayBoard[4][4].color == 0 -> setPiece(arrayBoard[4][4],turn)
-                arrayBoard[4][3].color == 0 -> setPiece(arrayBoard[4][3],turn)
-                arrayBoard[4][2].color == 0 -> setPiece(arrayBoard[4][2],turn)
-                arrayBoard[4][1].color == 0 -> setPiece(arrayBoard[4][1],turn)
-                arrayBoard[4][0].color == 0 -> setPiece(arrayBoard[4][0],turn)
-            }
-            
-        }
-        arrayArrows[5].setOnClickListener {
-            when {
-                arrayBoard[5][5].color == 0 -> setPiece(arrayBoard[5][5],turn)
-                arrayBoard[5][4].color == 0 -> setPiece(arrayBoard[5][4],turn)
-                arrayBoard[5][3].color == 0 -> setPiece(arrayBoard[5][3],turn)
-                arrayBoard[5][2].color == 0 -> setPiece(arrayBoard[5][2],turn)
-                arrayBoard[5][1].color == 0 -> setPiece(arrayBoard[5][1],turn)
-                arrayBoard[5][0].color == 0 -> setPiece(arrayBoard[5][0],turn)
-            }
-            
-        }
-        arrayArrows[6].setOnClickListener {
-            when {
-                arrayBoard[6][5].color == 0 -> setPiece(arrayBoard[6][5],turn)
-                arrayBoard[6][4].color == 0 -> setPiece(arrayBoard[6][4],turn)
-                arrayBoard[6][3].color == 0 -> setPiece(arrayBoard[6][3],turn)
-                arrayBoard[6][2].color == 0 -> setPiece(arrayBoard[6][2],turn)
-                arrayBoard[6][1].color == 0 -> setPiece(arrayBoard[6][1],turn)
-                arrayBoard[6][0].color == 0 -> setPiece(arrayBoard[6][0],turn)
-            }
-            
-        }
-
-
-    }
-
-    private fun setPiece(square : Square, color : Int){
-        square.color = color
-        when(color){
-            1 -> square.image.setImageResource(R.drawable.ficha_roja)
-            2 -> square.image.setImageResource(R.drawable.ficha_amarilla)
-        }
-        changeTurn()
     }
 
     private fun setArrays() {
@@ -173,5 +268,20 @@ class GameActivity : AppCompatActivity() {
     private fun setScore() {
         binding.btnRedScore.text = redScore.toString()
         binding.btnYellowScore.text = yellowScore.toString()
+    }
+
+    fun setKonfetti(konfettiView : KonfettiView, color : Int){
+        var konfettiColor : Int
+        if (color==1) konfettiColor = Color.RED else konfettiColor = Color.YELLOW
+        konfettiView.build()
+            .addColors(konfettiColor)
+            .setDirection(0.0, 359.0)
+            .setSpeed(1f, 5f)
+            .setFadeOutEnabled(true)
+            .setTimeToLive(1000L)
+            .addShapes(Shape.Square, Shape.Circle)
+            .addSizes(Size(12))
+            .setPosition(-50f, konfettiView.width + 50f, -50f, -50f)
+            .streamFor(300, 2000L)
     }
 }
